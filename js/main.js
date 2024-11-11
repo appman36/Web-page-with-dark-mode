5008ec09b2e1886eab9c2f0ea05e3238bc8a6050
appman36 <goollepi199@gmail.com>
2024-11-11 22:23:12 +0700

    2nd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
-const buttonDarkMode = document.querySelector('.dark-mode-button');
+const buttonNightMode = document.querySelector(".dark-mode-button");
 
-buttonDarkMode.onclick = function () {
-    buttonDarkMode.classList.toggle("dark-mode-button--active");
+// Проверка темной темы на уровне системных настроек
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    buttonNightMode.classList.add("dark-mode-button--active");
+    document.body.classList.add('night');
 }
+
+// Проверка темной темы в localStorage
+
+if (localStorage.getItem('darkMode') === 'night') {
+    buttonNightMode.classList.add("dark-mode-button--active");
+    document.body.classList.add('night');
+}
+
+// Если меняются системные настройки, то меняем тему
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', () => {
+    const newColorScheme = event.matches ? "night" : 'light';
+
+    if (newColorScheme === 'night') {
+        buttonNightMode.classList.add("dark-mode-button--active");
+        document.body.classList.add('night');
+        localStorage.setItem("nightMode", "light");
+    } else {
+        buttonNightMode.classList.remove("dark-mode-button--active");
+        document.body.classList.remove('night');
+    }
+})
+
+// Ночной режим по кнопке
+buttonNightMode.onclick = function () {
+    buttonNightMode.classList.toggle("dark-mode-button--active");
+    const isNight = document.body.classList.toggle('night');
+
+    if (isNight) {
+        localStorage.setItem('nightMode', 'night')
+    } else {
+        localStorage.setItem("nightMode", "light");
+    }
+}
+
